77d3a4b4c8d40f2397146063a0bddeae9d204001
nurali <nmakhmutzhan04@gmail.com>
2023-04-09 15:33:10 +0600

    1

--- Main.java
+++ Main.java
@@ -12,20 +12,12 @@ public class Main {
                 for (int i = 0; i < n1; i++) {
                     numbers1[i] = scanner.nextInt();
                 }
-                System.out.println(problem1(numbers1));
-                break;
-            case 2:
-                int n2 = scanner.nextInt();
-                int[] numbers2 = new int[n2];
-                for (int i = 0; i < n2; i++ ){
-                    numbers2[i] = scanner.nextInt();
-                }
-                System.out.println(problem2(numbers2));
+                System.out.println(find_minimum(numbers1));
                 break;
 
-        }
     }
-    public static int problem1(int[] numbers){
+    }
+    public static int find_minimum(int[] numbers){
         int min = numbers[0];
         for (int j = 1; j < numbers.length; j++) {
             if (numbers[j] < min) {
@@ -34,12 +26,5 @@ public class Main {
         }
         return min;
     }
-    public static double problem2(int[] numbers){
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
-        }
 
-        return (double) sum / numbers.length;
-    }
-    }
+}
